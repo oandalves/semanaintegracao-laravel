509bb63e96808c679b767ef23fe14f246eaf6b82
Roger Mathews <roger@interative.info>
2017-10-25 00:59:49 -0200

    rotas

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(array('prefix' => 'admin', 'middleware' => ['auth'], ['namespace' => 'admin'], 'as' => 'admin.'), function() {
+
+	Route::get('/', 						'Admin\HomeController@index')->name('index');
+	Route::get('/posts', 					'Admin\PostsController@index')->name('posts.index');
+	Route::get('/posts/novo', 				'Admin\PostsController@create')->name('posts.create');
+	Route::post('/posts/novo/salvar', 		'Admin\PostsController@store')->name('posts.create.save');
+	Route::get('/posts/editar/{id}', 		'Admin\PostsController@edit')->name('posts.edit');
+	Route::post('/posts/update/{id}', 		'Admin\PostsController@update')->name('posts.edit.update');
+	Route::post('/posts/delete/{id}', 		'Admin\PostsController@destroy')->name('posts.delete');
+});
+
+Route::get('/', 				'HomeController@index')->name('home');
+Route::get('/posts', 			'PostsController@index')->name('posts.index');
+Route::get('/posts/ver/{id}', 	'PostsController@show')->name('posts.show');

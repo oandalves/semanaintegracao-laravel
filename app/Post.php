dcf5924778138702da5193a59b41d960eb9a1fe6
Roger Mathews <roger@interative.info>
2017-10-25 00:58:48 -0200

    models

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model {
+	use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+	
+	protected $table = 'posts';
+
+	protected $fillable = [
+        'titulo', 'conteudo', 'usuario_id',
+    ];
+}
